89485ceef4388b373d021e6c9ddc2930486a85cc
TheBrain08 <t-bilharz@online.de>
2023-10-15 22:03:25 +0200

    adding userinfo command

---
+++ cogs/userinfo_command.py
@@ -0,0 +1,35 @@
+import datetime
+
+import discord
+from discord.ext import commands
+
+
+class UserInfoCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.Cog.slash_command(name='userinfo', description='Bekomme Info\'s über einen User ')
+    async  def userinfo_command(self, ctx, user: discord.Member = None):
+        user = user or ctx.author
+
+        embed = discord.Embed(title=f'User Info - {user}', color=user.top_role.color)
+        embed.set_thumbnail(url=user.avatar_url)
+        embed.add_field(name='ID', value=user.id)
+        embed.add_field(name='Nickname', value=user.display_name)
+        embed.add_field(name='Account erstellt', value=discord.utils.styled_timestamp(user.created_at,
+                                                                                      style=discord.TimestampStyle.relative))
+        embed.add_field(name='Server beigetreten', value=discord.utils.styled_timestamp(user.joined_at,
+                                                                                      style=discord.TimestampStyle.relative))
+        roles = [role.mention for role in user.roles if role != user.guild.default_role]
+        roles_string = ', '.join(roles) if roles else 'None'
+        embed.add_field(name='Rollen', value=roles_string)
+        embed.add_field(name='Höchste Rolle', value=user.top_role.mention)
+        embed.timestamp = datetime.datetime.utcnow()
+        embed.set_footer(text=f'Angefragt von {ctx.author}', icon_url=ctx.author.avatar_url)
+
+        await ctx.respond(embed=embed, hidden=True)
+
+
+def setup(bot):
+    bot.add_cog(UserInfoCommand(bot))
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 import os
+from pathlib import Path
 
-import  discord
+import discord
 from discord.ext import  commands
 
 from dotenv import  load_dotenv
@@ -16,11 +17,17 @@ bot = commands.Bot(
     intents=discord.Intents.default(),
     activity=discord.Activity(type=discord.ActivityType.playing, name='auf AI WS23/24!'),
     status=discord.Status.online,
-    sync_commands=False,
+    sync_commands=True,
     delete_not_existing_commands=False
 )
 
 if __name__ == '__main__':
     log.info('Starting bot...')
+    cogs = [file.stem for file in Path('cogs').glob('**/*.py') if not file.name.startswith('__')]
+    log.info(f'Loading {len(cogs)} cogs...')
+
+    for cog in cogs:
+        bot.load_extension(f'cogs.{cog}')
+        log.info(f'Loaded cog {cog}')
     token = os.getenv('BOT_TOKEN')
     bot.run(token)
