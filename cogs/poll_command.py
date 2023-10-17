d4650733351fb25f66894135f1f49f48e00050fa
TheBrain08 <t-bilharz@online.de>
2023-10-17 15:59:49 +0200

    repair the poll command

--- cogs/poll_command.py
+++ cogs/poll_command.py
@@ -10,17 +10,16 @@ class PollCommand(commands.Cog):
         self.bot = bot
 
     @commands.Cog.slash_command(name='Poll', description='Starte eine Umfrage')
-    async def poll_command(self, ctx, *args):
-
+    async def poll_command(self, ctx, arg1):
         embed = discord.Embed(title=f'Umfrage 📊',
-                              description=f'{" ".join(args)}', color='red')
-        #embed.set_thumbnail(url="https://i.ibb.co/yy80FHf/analytics-max-removebg-preview.png")
+                              description=f'{arg1}', color=16711680)
+        embed.set_thumbnail(url="https://i.ibb.co/yy80FHf/analytics-max-removebg-preview.png")
         embed.timestamp = datetime.datetime.utcnow()
         embed.set_footer(text=f'Erstellt von {ctx.author} • ', icon_url=ctx.author.avatar_url)
 
-        await ctx.respond(embed=embed)
-        """await message.add_reaction('✔️')
-        await message.add_reaction('❌')"""
+        message = await ctx.respond(embed=embed)
+        await message.add_reaction('✔️')
+        await message.add_reaction('❌')
 
 
 
