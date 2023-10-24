181597964619a010e52748b44e14c95112a78479
TheBrain08 <t-bilharz@online.de>
2023-10-24 16:55:41 +0200

    .

--- cogs/essen_menu.py
+++ cogs/essen_menu.py
@@ -5,27 +5,31 @@ import discord
 from discord.ext import commands
 from datetime import datetime, timedelta, date
 
-wochentage = ['tab-mon','tab-tue','tab-wed','tab-thu','tab-fri']
+from main import bot
+
+wochentage = ['tab-mon', 'tab-tue', 'tab-wed', 'tab-thu', 'tab-fri']
+
 
 class EssenMenu(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     @commands.Cog.slash_command(name='menu', description='Bekomme den Speißeplan gesendet ')
-    async def menu_command(self, ctx):
+    # @commands.Cog.listener()
+    async def on_ready(self, ctx):  # ctx entfernen für später
+        # channel = self.bot.get_channel(954094356988563467)
         my_dt = datetime(2024, 6, 21, 10, 12, 53)
         my_dt_trunc = date(my_dt.year,
-                                    my_dt.month,
-                                    my_dt.day)
+                           my_dt.month,
+                           my_dt.day)
         start_of_week = my_dt_trunc - timedelta(days=my_dt_trunc.weekday())
 
         end_of_week = start_of_week + timedelta(days=6)
         for x in wochentage:
-
-            embed = discord.Embed(title='Essen Menu von ' + str(open('./Essen/' + x + '-tag.txt', 'r').read()), color=discord.Color.blurple())
+            embed = discord.Embed(title='Essen Menu von ' + str(open('./Essen/' + x + '-tag.txt', 'r').read()),
+                                  color=discord.Color.blurple())
             embed.add_field(name='Essen 1',
-                            value='```'+ open('./Essen/' + x + '0.txt', 'r', encoding='windows-1252').read() + '```')
+                            value='```' + open('./Essen/' + x + '0.txt', 'r', encoding='windows-1252').read() + '```')
             embed.add_field(name='Essen 2',
                             value='```' + open('./Essen/' + x + '1.txt', 'r', encoding='windows-1252').read() + '```')
             embed.add_field(name='Buffet',
@@ -33,7 +37,7 @@ class EssenMenu(commands.Cog):
 
             embed.timestamp = datetime.utcnow()
 
-            await ctx.respond(embed=embed)
+            await ctx.respond(embed=embed)  # await channel.send(embed=embed)
 
 
 def setup(bot):
