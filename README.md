8e63a1a3d07b93e177576fe64f4ab1d9252c7662
TheBrain08 <t-bilharz@online.de>
2024-10-23 14:25:32 +0200

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# hochschul-bot-py
-This is an inofificial Discord bot for WS23/24 AI at Offenburg University of Applied Sciences.
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import discord
 from discord.ext import commands
 
 from dotenv import load_dotenv
-#from Essen import scratchdelet
 
 load_dotenv()
 
@@ -23,8 +22,6 @@ bot = commands.Bot(
     sync_commands=True,
     delete_not_existing_commands=True
 )
-#scratchdelet.start()
-
 if __name__ == '__main__':
     log.info('Starting bot...')
     cogs = [file.stem for file in Path('cogs').glob('**/*.py') if not file.name.startswith('__')]
